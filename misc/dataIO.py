4c0b02325e14dd31ea60bd63f63fc239b6282509
Daigo HIROOKA <tappy.hello-planet@ezweb.ne.jp>
2017-11-17 06:13:50 +0900

    add food-101 dataset handling script

--- misc/dataIO.py
+++ misc/dataIO.py
@@ -1,12 +1,13 @@
 # coding:utf-8
 
+import json
 import numpy as np
 import pandas as pd
 from tqdm import tqdm
 from glob import glob
 import pdb
 
-from .utils import *
+from utils import *
 
 class InputSampler:
 
@@ -98,4 +99,87 @@ class LabelfileLoader(object):
         with open(filepath, 'rb') as f:
             label = pd.read_csv(f, delim_whitespace=True, header=1)
         return label
+
+class Food101Sampler(object):
+    def __init__(self,
+                 metadir,
+                 target_size = 256,
+                 image_size = 128,
+                 split = 5,
+                 num_utilize = np.inf):
+        self.metadir = metadir
+        self.target_size = target_size
+        self.image_size = image_size
+        self.split = split
+
+        with open(metadir + '/train.json') as f:
+            train_json = json.load(f)
+        with open(metadir + '/test.json') as f:
+            test_json = json.load(f)
+            
+        classes = []
+        for k in train_json.keys():
+            classes.append(k)
+        classes.sort()
+        self.classes = np.array(classes)
+            
+        self.train_image_paths = []
+        self.train_labels = pd.DataFrame(columns = self.classes)
+        self.test_image_paths = []
+        self.test_labels = pd.DataFrame(columns = self.classes)
+
+        image_dir = metadir.replace('meta', 'images')
+        convert_path = lambda path_:image_dir + '/' + path_ + '.jpg'
+        for food in self.classes:
+            self.train_image_paths.append(list(map(convert_path, train_json[food])))
+            train_labels = pd.DataFrame(np.zeros(shape = (750, 101)), columns = classes)
+            train_labels[food] = 1
+            self.train_labels = pd.concat([self.train_labels, train_labels], axis = 0,
+                                          ignore_index = True)
+
+            self.test_image_paths.append(list(map(convert_path, test_json[food])))
+            test_labels = pd.DataFrame(np.zeros(shape = (250, 101)), columns = classes)
+            test_labels[food] = 1
+            self.test_labels = pd.concat([self.test_labels, test_labels], axis = 0,
+                                         ignore_index = True)
+
+        self.train_image_paths = np.array(self.train_image_paths).flatten()
+        self.data_size = min(len(self.train_image_paths), num_utilize)
+        permute_idx = np.random.permutation(self.data_size)
+        self.train_image_paths = self.train_image_paths[permute_idx]
+        self.train_labels = np.array(self.train_labels)[permute_idx]
+
+        self.test_image_paths = np.array(self.test_image_paths).flatten()
+        permute_idx = np.random.permutation(len(self.test_image_paths))
+        self.test_image_paths = self.test_image_paths[permute_idx]
+        self.test_labels = np.array(self.test_labels)[permute_idx]
+
+        print('train size : {}, test size : {}'.format(self.data_size, len(self.test_image_paths)))
+
+        self.data = None
+        self.split_label = None
+
+    def reload(self):
+        split_idx = np.random.choice(np.arange(self.data_size),
+                                     int(self.data_size/self.split),
+                                     replace = False)
+        split_paths = self.train_image_paths[split_idx]
+        print('train data loading')
+        self.data = np.array([get_image(path, self.target_size, self.image_size)
+                              for path in tqdm(split_paths)])
+        self.split_labels = self.train_labels[split_idx]
+
+    def sample(self, batch_size):
+        sample_idx = np.random.choice(len(self.data.index),
+                                      batch_size,
+                                      replace = True)
+        images = self.data[sample_idx]
+        labels = self.split_labels[sample_idx]
+        return images, labels
+
+    def load_testset(self):
+        test_images = np.array([get_image(path, self.target_size, self.image_size)
+                                for path in tqdm(self.test_image_paths)])
+        return test_images, test_labels
+
 
